195ce386d71e6f88731583dfcffb7b344da3a3cf
ViniciusTech <145513542+ViniiciusTech@users.noreply.github.com>
2025-10-22 00:19:48 -0300

    feat: adiciona configuração base de cores e integração inicial com Firebase

---
+++ constants/colors.js
@@ -0,0 +1,23 @@
+import { InferenceMode } from "firebase/ai";
+
+export const COLORS = {
+    // Cor primaria 
+    PRIMARY: '#FF69B4',
+
+    // Cores neutras 
+    white: "#FFFFFF",
+    black: "#000000",
+
+    // Tons de cinza - textos secundarios e fundos
+    gray: " #999999",
+    grayMedium: "#CCCCCC",
+    grayLight: "#F5F5F5",
+
+    // Cores de fedback - msg e status
+    success: "#4CAF50", 
+    error: "#F44336",
+    warning: "#FF9800", 
+    info: "#2196F3",
+
+
+}
---
+++ constants/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  // Substitua estes valores com os dados do seu projeto Firebase
+  apiKey: "AIzaSyCm3QUXHFfUH8kDqrFH7fJX4Y5qE9QU0To",                    // Chave de API
+  authDomain: "confeitaria-app-6024e.firebaseapp.com",            // Domínio de autenticação
+  projectId: "confeitaria-app-6024e",              // ID do projeto
+  storageBucket: "confeitaria-app-6024e.firebasestorage.app",      // Bucket de armazenamento
+  messagingSenderId: "336480979951",  // ID do remetente
+  appId: "1:336480979951:web:f2b339464c31d496f4441a",                      // ID do aplicativo
+}
